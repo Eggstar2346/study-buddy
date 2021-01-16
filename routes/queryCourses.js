bd7c10c1bdc43dd082fb4082a47f2a27d2dbcdf2
YuYing_Liang <yuying.liang00@gmail.com>
2021-01-16 13:54:05 -0500

    added endpoints for queries

--- routes/queryCourses.js
+++ routes/queryCourses.js
@@ -1,3 +1,74 @@
 module.exports = (app, pool) => {
-    
+
+    app.post('/courses/addCourse', async(req, res) => {
+        const {course_id, student_id, profs, profs_email, tas, ta_emails, description, course_name, grade, type, priority, timetable} = req.body
+        const response = await pool.query(`
+            INSERT INTO courses (course_id, student_id, profs, profs_email, tas, ta_emails, desr, course_name, grade, type, priority, timetable) 
+            VALUES (${course_id}, ${student_id}, ${profs}, ${profs_email}, ${tas}, ${ta_emails}, ${description}, ${course_name}, ${grade}, ${type}, ${course_priority}, ${timetable})
+        `)
+        console.log(response)
+        if(response) {
+            res.send('created course!')
+        }
+    })
+
+    app.get('/courses/getCourseDetails', async(req, res) => {
+        const sid = req.body
+        const response = await pool.query(`
+            SELECT * FROM courses WHERE student_id = ${sid}
+        `)
+        console.log(response.rows[0])
+        if(response.rows[0]) {
+            console.log('retrieved course ' + response.rows[0]['course_name'])
+            res.send(response.rows[0])
+        }
+    })
+
+    // called everytime a new grade in tasks is entered
+    app.post('/courses/updateGrade', async(req, res) => {
+        const { grade, grade_weight, course_id } = req.body
+        const response = await pool.query(`
+            UPDATE courses SET grade =
+            SELECT sum(${grade}) / sum(${grade_weight}) * 100 FROM tasks
+            WHERE course_id = ${course_id} AND grade IS NOT NULL
+            GROUP BY ${course_id}
+        `)
+        console.log(response)
+    })
+
+    app.get('/courses/getGrade', async(req, res) => {
+        const {course_id, student_id} = req.body 
+        const response = await pool.query(`
+            SELECT grade FROM courses 
+            WHERE course_id = ${course_id} AND student_id = ${student_id}
+        `)
+        console.log(response, response.rows)
+    })
+
+    app.get('/courses/getPriority', async(req, res) => {
+        const {course_id, student_id} = req.body 
+        const response = await pool.query(`
+            SELECT course_priority FROM courses 
+            WHERE course_id = ${course_id} AND student_id = ${student_id};
+        `)
+        console.log(response, response.rows)
+    })
+
+    app.get('/courses/getTimetable', async(req, res) => {
+        const {course_id, student_id} = req.body 
+        const response = await pool.query(`
+            SELECT timetable FROM courses 
+            WHERE course_id = ${course_id} AND student_id = ${student_id}
+        `)
+        console.log(response, response.rows)
+    })
+
+    app.get('/courses/removeCourse', async(req, res) => {
+        const course_id = req.body 
+        const response = await pool.query(`
+            DELETE FROM courses WHERE course_id = ${course_id}
+        `)
+        console.log(response, response.rows)
+    })
+
 }
--- routes/queryTasks.js
+++ routes/queryTasks.js
@@ -1,3 +1,48 @@
 module.exports = (app, pool) => {
     
+    app.post('/tasks/addTask', async(req, res) => {
+        const {task_id, course_id, task_type, due_date, completed, grade_weight} = req.body
+        const response = await pool.query(`
+            INSERT INTO tasks (task_id, course_id, task_type, due_date, completed, grade, grade_weight, score, time_completed) 
+            VALUES (${task_id}, ${course_id}, ${task_type}, ${due_date}, ${completed}, NULL, ${grade_weight}, NULL, NULL)
+        `)
+        console.log(response)
+        if(response) {
+            res.send('created tasks!')
+        }
+    })
+
+    app.get('/tasks/getGrade', async(req, res) => {
+        const task_id = req.body 
+        const response = await pool.query(`
+            SELECT grade FROM tasks WHERE task_id = ${task_id}
+        `)
+        console.log(response, response.rows)
+    })
+
+
+    app.get('/tasks/getDueDateAndGradeWeight', async(req, res) => {
+        const task_id = req.body 
+        const response = await pool.query(`
+            SELECT due_date, grade_weight FROM TASKS WHERE task_id = ${task_id}
+        `)
+        console.log(response, response.rows)
+    })
+
+    app.post('/tasks/setScore', async(req, res) => {
+        const {task_id, score} = req.body 
+        const response = await pool.query(`
+            UPDATE tasks SET score = ${score} WHERE task_id = ${task_id}
+        `)
+        console.log(response, response.rows)
+    })
+
+    app.post('/tasks/setCompleted', async(req, res) => {
+        const task_id = req.body 
+        const response = await pool.query(`
+            UPDATE tasks SET completed = TRUE WHERE task_id = ${task_id};
+        `)
+        console.log(response, response.rows)
+    })
+
 }
