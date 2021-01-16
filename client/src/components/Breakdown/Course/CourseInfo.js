4b5355e2f79f41d1f147398d712826acc51a42bc
John Lee <johnlee832@gmail.com>
2021-01-16 02:22:31 -0500

    Initiated course breakdown structure

---
+++ client/src/components/Breakdown/Course/CourseInfo.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+const CourseInfo = props => {
+    const [profName, setProfName] = useState("Stark Draper");
+    const [profEmail, setProfEmail] = useState("stark.draper@utoronto.ca")
+    const [TAs, setTas] = useState()
+};
+
+export default CourseInfo;
---
+++ client/src/components/Breakdown/Course/Dates.js
@@ -0,0 +1,7 @@
+import React, {useState} from 'react';
+
+const Dates = props => {
+    
+};
+
+export default Dates;
