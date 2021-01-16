d738ff19eed2900ae74aa9c7acfe8e040a75559b
YuYing_Liang <yuying.liang00@gmail.com>
2021-01-16 02:27:16 -0500

    added dashboard shtuff

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import "shards-ui/dist/css/shards.min.css"
 import styles from './App.module.css';
 import Dashboard from './containers/Dashboard';
 import Navbar from "./components/Navbar";
+import Breakdown from "./containers/Breakdown";
 
 function App() {
   return (
@@ -13,8 +14,8 @@ function App() {
       <Navbar/>
       <Switch>
         <Route exact path="/" component={Dashboard}/>
-        {/* <Route exact path="/courses" component={}/>
-        <Route exact path="/analytics" component={}/>
+        <Route exact path="/courses" component={Breakdown}/>
+        {/*<Route exact path="/analytics" component={}/>
         <Route exact path="/settings" component={}/> */}
       </Switch>
       {/* Footer */}
--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -1,20 +1,41 @@
 import React from 'react';
-import { Col, Container, Nav, NavItem, NavLink, Row } from "shards-react";
+import { Card, Col, Container, Row, CardBody, CardTitle, CardSubtitle, Badge, CardHeader } from "shards-react";
 
 function Dashboard (props) {
-    const daysOfWeek = ['mon','tues','wed','thurs','fri'] //placeholder
+    const daysOfWeek = ['mon','tues','wed','thurs','fri', 'weekend'] //placeholder
+    const tags = ['ESC301', 'AER372', 'CSC384', 'ECE353', 'MIE438', 'ROB313'] //placeholder
 
     return(
         <Container>
+            <br/><br/>
             <Row>
-                {
-                    daysOfWeek.map((val,i) => {
-                        return <Col>
-                            <h4>{val}</h4>
-                        </Col>
-                    })
-                }
+                <Col lg={9}>
+                    <Container style={{overflow: 'auto'}}>
+                        <Row style={{width: '180%'}}>
+                            {
+                                daysOfWeek.map((val,i) => {
+                                    return <Col md={2}>
+                                        <Card>
+                                            <CardHeader style={{textAlign: 'center', textTransform: 'uppercase'}}>{val}</CardHeader>
+                                            <CardBody>
+                                                {
+                                                    tags.map(val => {
+                                                        return<Badge>{val}</Badge>
+                                                    })
+                                                }
+                                            </CardBody>
+                                        </Card>
+                                    </Col>
+                                })
+                            }
+                        </Row>
+                    </Container>
+                </Col>
+                <Col lg={3}>
+
+                </Col>
             </Row>
+        
         </Container>
     )
 }
